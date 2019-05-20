fc82eadeaa3384246376f9d3d1bdb5b4bdb9dd87
McPrapor <max.terleckiy@gmail.com>
2019-05-20 10:51:08 +0200

    Update and rename metal-vendor-blobs.mk to htc_v36bml_dugl-vendor-blobs.mk

---
+++ htc_v36bml_dugl-vendor-blobs.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc_v36bml_dugl/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/htc_v36bml_dugl/vendor,system/vendor)
--- metal-vendor-blobs.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/ulefone/metal/vendor,system/vendor)
